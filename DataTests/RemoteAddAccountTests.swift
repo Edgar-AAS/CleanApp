9ea0ae579f84b46c91d05a996418ec23d8221b55
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-10 14:24:13 -0300

    move SUT cration to a factory helper method

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -27,16 +27,15 @@ protocol HttpPostClient {
 
 class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url() {
+        //todos os teste devem ter acesso ao httpClientSpy
         let url = URL(string: "http://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel: makeAddAccountModel())
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut()
         let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel)
         let data = try? JSONEncoder().encode(addAccountModel)
@@ -48,6 +47,14 @@ extension RemoteAddAccountTests {
     //Design pattern Factory
     //Se o AddAccountModel mudar seus parametros no futuro
     //evita que que o seja necessario trocar todos os testes que o tenha
+    //como eu preciso ter acesso ao meu spy para fazer a comparação dos testes
+    //cria um factory do meu sut passando a tupla (sut, httpClientSpy) como retorno
+    
+    func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy)  {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
     
     func makeAddAccountModel() -> AddAccountModel {
         return AddAccountModel(
@@ -57,7 +64,6 @@ extension RemoteAddAccountTests {
             passwordConfirmation: "password123"
         )
     }
-    
     
     class HttpClientSpy: HttpPostClient {
         var url: URL?
