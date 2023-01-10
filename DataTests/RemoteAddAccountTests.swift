23955cedf697e65a54dbc89216d580290579a0fe
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-10 12:51:52 -0300

    move AddAccountModel creation to a helper method

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -30,23 +30,35 @@ class RemoteAddAccountTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        let addAccountModel = AddAccountModel(name: "paulo", email: "paulo@outlook.com", password: "password123", passwordConfirmation: "password123")
-        sut.add(addAccountModel: addAccountModel)
+        sut.add(addAccountModel: makeAddAccountModel())
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
-        let addAccountModel = AddAccountModel(name: "paulo", email: "paulo@outlook.com", password: "password123", passwordConfirmation: "password123")
+        let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel)
-        
         let data = try? JSONEncoder().encode(addAccountModel)
         XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
 extension RemoteAddAccountTests {
+    //Design pattern Factory
+    //Se o AddAccountModel mudar seus parametros no futuro
+    //evita que que o seja necessario trocar todos os testes que o tenha
+    
+    func makeAddAccountModel() -> AddAccountModel {
+        return AddAccountModel(
+            name: "paulo",
+            email: "paulo@outlook.com",
+            password: "password123",
+            passwordConfirmation: "password123"
+        )
+    }
+    
+    
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         var data: Data?
