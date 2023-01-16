70c0579939401f2f593080144b1c54f7fc02d6aa
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-16 17:48:06 -0300

    ensure AlamofireAdapter calls Alamofire with correct data

--- DataTests/Helpers/TestFactorys.swift
+++ DataTests/Helpers/TestFactorys.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data {
     return Data("invalid_data".utf8)
 }
 
+func makeValidData() -> Data {
+    return Data("{\"name\":\"Edgar\"}".utf8)
+}
+
 func makeUrl() -> URL {
     return URL(string: "http://any-url.com")!
 }
