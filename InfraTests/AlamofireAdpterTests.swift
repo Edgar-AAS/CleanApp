547bbc2516c48cd1e9ec5bdea0ba6a918d7cd3c7
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-16 18:19:56 -0300

    move duplicated code to a helper method to improve readability

--- InfraTests/AlamofireAdpterTests.swift
+++ InfraTests/AlamofireAdpterTests.swift
@@ -17,29 +17,17 @@ class AlamofireAdapter {
 class AlamofireAdpterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
-        let sut = makeSut()
-        sut.post(to: url, with: makeValidData())
-        let exp = expectation(description: "waiting")
-        
-        UrlProtocolStub.observerRequest { (request) in
+        testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
     
     func test_post_should_make_request_with_no_data() {
-        let sut = makeSut()
-        sut.post(to: makeUrl(), with: nil)
-        let exp = expectation(description: "waiting")
-        
-        UrlProtocolStub.observerRequest { (request) in
+        testRequestFor(data: nil) { (request) in
             XCTAssertNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -50,6 +38,18 @@ extension AlamofireAdpterTests {
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         return AlamofireAdapter(session: session)
+    }
+    
+    func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.post(to: url, with: data)
+        let exp = expectation(description: "waiting")
+        
+        UrlProtocolStub.observerRequest { (request) in
+            action(request)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
