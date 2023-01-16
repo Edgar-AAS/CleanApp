ef97c9c4df25308da70b64a8c04502cc7fbf1b7c
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-16 17:25:40 -0300

    improve test name

--- InfraTests/AlamofireAdpterTests.swift
+++ InfraTests/AlamofireAdpterTests.swift
@@ -14,7 +14,7 @@ class AlamofireAdapter {
 }
 
 class AlamofireAdpterTests: XCTestCase {
-    func test_() {
+    func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
@@ -28,11 +28,9 @@ class AlamofireAdpterTests: XCTestCase {
             XCTAssertEqual("POST", request.httpMethod)
             exp.fulfill()
         }
-        
         wait(for: [exp], timeout: 1)
     }
 }
-
 
 class UrlProtocolStub: URLProtocol {
     static var emit: ((URLRequest) -> Void)?
