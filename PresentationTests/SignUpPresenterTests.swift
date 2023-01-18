faa55959f0e45b890f1831beb5b7b94a0df76fe3
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-17 22:36:30 -0300

    move SUT creation to a factory method

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -33,8 +33,7 @@ struct SignUpViewModel {
 
 class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_messsage_if_name_is_not_provided() {
-        let alertViewSpy = AlertViewSpy()
-        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let (sut, alertViewSpy) = makeSut()
         let signUpViewModel = SignUpViewModel(email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Erro de validação", message: "O campo nome é obrigatório"))
@@ -42,6 +41,12 @@ class SignUpPresenterTests: XCTestCase {
 }
 
 extension SignUpPresenterTests {
+    func makeSut() -> (sut: SignUpPresenter, alerviewSpy: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        return (sut, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
         var viewModel: AlertViewModel?
         func showMessage(viewModel: AlertViewModel) {
