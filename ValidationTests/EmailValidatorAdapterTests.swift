c1a8ec41989a6b8f1ccafd4bbfcac2c20578a35a
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-23 13:30:59 -0300

    move sut cration to a factory method

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -13,11 +13,26 @@ public class EmailValidatorAdapter: EmailValidator {
 
 class EmailValidatorAdapterTests: XCTestCase {
     func test_invalid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "rr"))
         XCTAssertFalse(sut.isValid(email: "rr@"))
         XCTAssertFalse(sut.isValid(email: "rr@rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr."))
         XCTAssertFalse(sut.isValid(email: "@rr.com"))
+    }
+    
+    func test_valid_emails() {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "edgar@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "edgar@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "edgar@outlook.com"))
+        XCTAssertTrue(sut.isValid(email: "edgar@live.de"))
+        XCTAssertTrue(sut.isValid(email: "edgar_almd@gmail.com"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
