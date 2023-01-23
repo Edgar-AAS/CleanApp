ad258f2882de488be4582d7aa7ba346cadde9adb
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-23 10:03:29 -0300

    ensure SignUpController calls signUp signUp when saveButton is tapped

--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -4,10 +4,20 @@ import Presentation
 
 final class SignUpViewController: UIViewController {
     @IBOutlet var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet var saveButton: UIButton!
+    var signUp: ((SignUpViewModel) -> Void)? //dependencia
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        configure()
+    }
+    
+    private func configure() {
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func saveButtonTapped() {
+        signUp?(SignUpViewModel(name: nil, email: nil, password: nil, passwordConfirmation: nil))
     }
 }
 
--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -15,13 +15,45 @@ class SignUpViewControllerTests: XCTestCase {
     func test_sut_implements_alertView() {
         XCTAssertNotNil(makeSut() as AlertView)
     }
+    
+    func test_saveButton_calls_signUp_on_tap() {
+        var callsCount = 0
+        
+        let signSpy: (SignUpViewModel) -> Void = { _ in
+            callsCount += 1
+        }
+        
+        let sut = makeSut(signUpSpy: signSpy)
+        
+        sut.saveButton.simulateTap()
+        XCTAssertEqual(callsCount, 1)
+        XCTAssertNotNil(makeSut() as AlertView)
+    }
 }
 
 extension SignUpViewControllerTests {
-    func makeSut() -> SignUpViewController {
+    func makeSut(signUpSpy: ((SignUpViewModel) -> Void)? = nil) -> SignUpViewController {
         let storyboard = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = storyboard.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        sut.signUp = signUpSpy
         sut.loadViewIfNeeded()
         return sut
     }
 }
+
+//simulando eventos
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { (target) in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
+
+
