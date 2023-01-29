a846db4d08378f4ca8fe18d40ad26c2f864cdc63
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-01-29 14:32:34 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL

--- Domain/UseCases/Authentication.swift
+++ Domain/UseCases/Authentication.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-//add account USE CASE 
-//o padrao do swift é internal que só é acessivel dentro do mesmo target
-//setando protocol e models para public
-//por padrao o construtor de uma struct é internal
-//para acessa-lo em outro target e preciso criar um novo construtor publico
-//retorno assincrono
-
-
 public protocol Authentication {
     typealias Result = Swift.Result<AccountModel, DomainError>
     func add(addAccountModel: AuthenticationModel, completion: @escaping (Result) -> Void)
