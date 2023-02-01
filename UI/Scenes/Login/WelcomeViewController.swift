4c01e90611a93c3139cc7a5a4df277da3b9d0b18
Edgar Arlindo <edgar_almeida159@outlook.com>
2023-02-01 16:24:07 -0300

    ensure WelcomeViewController calls signUp on signUpButton tapped

--- UI/Scenes/Login/WelcomeViewController.swift
+++ UI/Scenes/Login/WelcomeViewController.swift
@@ -5,7 +5,9 @@ public final class WelcomeViewController: UIViewController, Storyboarded {
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     
-    public var login: (() -> Void)? //click no botao, essa função deve chamada e o comportamento nao depende desse controller, motivo: Nao acoplagem de componentes dentro da controller, a viewController deve ser burra em relação a qual tela chamar e a quais dependencias devem ser injetadas para o roteamento.
+    //click no botao, essa função deve chamada e o comportamento nao depende desse controller, motivo: Nao acoplagem de componentes dentro da controller, a viewController deve ser burra em relação a qual tela chamar e a quais dependencias devem ser injetadas para o roteamento.
+    public var login: (() -> Void)?
+    public var signUp: (() -> Void)?
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,10 +18,15 @@ public final class WelcomeViewController: UIViewController, Storyboarded {
         title = "4Devs"
         loginButton.layer.cornerRadius = 5
         loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        hideKeyboardTap()
+        signUpButton.layer.cornerRadius = 5
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
     }
     
     @objc private func loginButtonTapped() {
         login?()
+    }
+    
+    @objc private func signUpButtonTapped() {
+        signUp?()
     }
 }
